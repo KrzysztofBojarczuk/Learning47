876618b60343da36048661cee8f5e99c9865acb6
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-06 09:36:38 +0200

    Add project files.

---
+++ Miesięczny raport sprzedaży/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Miesięczny_raport_sprzedaży
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadź wartość miesięczną sprzedaży.");
+            double sales = double.Parse(Console.ReadLine());
+
+            double countyTax = sales * 0.02;
+            double stateTax = sales * 0.04;
+
+            double totalTax = countyTax + stateTax;
+
+
+            Console.WriteLine($" Wartość podateku naliczonego w hrabstwie wynosi {countyTax}.");
+            Console.WriteLine($"Wartość podateku w stanie wynosi {stateTax}.");
+            Console.WriteLine($"Całkowita wartość podatku wynosi {totalTax}.");
+        }
+    }
+}
